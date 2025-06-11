b629a872c17e5700fbc6b2c6e470e776f971bda1
Ahmad Hafizh Assaad <71488@fif.co.id>
2025-06-11 14:35:44 +0700

    :white_check_mark: add Factorial Exercise

--- exercise/day  1/controlFlow/src/main/java/org/example/Main.java
+++ exercise/day  1/controlFlow/src/main/java/org/example/Main.java
@@ -1,48 +1,60 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // if else statement
-        int number = -1;
-        if (number > 0) {
-            System.out.println( number + " is positive.");
-        } else if (number < 0) {
-            System.out.println( number + " is negative.");
-        } else {
-            System.out.println( number + " is zero.");
+        for (int i = 2; i <= 20; i += 2) {
+            long factorial = 1;
+
+            for (int j = 1; j <= i; j++) {
+                factorial *= j;
+            }
+
+            System.out.println("Factorial of " + i + " = " + factorial);
         }
-
-        int day = 3;
-        String dayName;
-
-        // switch case statement
-        switch (day){
-            case 1: dayName = "Monday"; break;
-            case 2: dayName = "Tuesday"; break;
-            case 3: dayName = "Wednesday"; break;
-            case 4: dayName = "Thursday"; break;
-            case 5: dayName = "Friday"; break;
-            case 6: dayName = "Saturday"; break;
-            case 7: dayName = "Sunday"; break;
-            default: dayName = "Invalid day"; break;
-        }
-        System.out.println(dayName);
-
-        // while loop
-        int i = 2;
-        while (i <= 10) {
-            System.out.println("Hello World - " + i);
-            i += 2;
-        }
-
-        //do while loop
-        int j = 1;
-        do {
-            System.out.println("Peserta - " + j);
-            j += 2;
-        } while (j <= 10);
     }
 }
+
+//public class Main {
+//    public static void main(String[] args) {
+//        // if else statement
+//        int number = -1;
+//        if (number > 0) {
+//            System.out.println( number + " is positive.");
+//        } else if (number < 0) {
+//            System.out.println( number + " is negative.");
+//        } else {
+//            System.out.println( number + " is zero.");
+//        }
+//
+//        int day = 3;
+//        String dayName;
+//
+//        // switch case statement
+//        switch (day){
+//            case 1: dayName = "Monday"; break;
+//            case 2: dayName = "Tuesday"; break;
+//            case 3: dayName = "Wednesday"; break;
+//            case 4: dayName = "Thursday"; break;
+//            case 5: dayName = "Friday"; break;
+//            case 6: dayName = "Saturday"; break;
+//            case 7: dayName = "Sunday"; break;
+//            default: dayName = "Invalid day"; break;
+//        }
+//        System.out.println(dayName);
+//
+//        // while loop
+//        int i = 2;
+//        while (i <= 10) {
+//            System.out.println("Hello World - " + i);
+//            i += 2;
+//        }
+//
+//        //do while loop
+//        int j = 1;
+//        do {
+//            System.out.println("Peserta - " + j);
+//            j += 2;
+//        } while (j <= 10);
+//    }
+//}
 
